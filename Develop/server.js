32c2c897f569bbcfc2c91f70dcecb27f17920237
Joshua Chu <jashya@gmail.com>
2020-04-19 15:57:29 -0400

    Fix stylesheet linking

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ var PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static(__dirname + '/public'));
 
 
 require("./routes/apiRoutes")(app);
