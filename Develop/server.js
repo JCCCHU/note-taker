a65db80666c937ae01fc8cb6474f21f6c0300c4e
Joshua Chu <jashya@gmail.com>
2020-07-06 21:19:48 -0400

    Add the assignemnt (as best as I could complete it)

--- Develop/server.js
+++ Develop/server.js
@@ -7,8 +7,8 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./Develop/routes/apiRoutes")(app);
+require("./Develop/routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
